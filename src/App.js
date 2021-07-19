77cb05eb874a9448448f2b974e03b78542b8dfbe
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-19 06:33:35 -0300

    :sparkles: add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <h1>My Contacts</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>My Contacts</h1>
+    </ThemeProvider>
   );
 }
 
