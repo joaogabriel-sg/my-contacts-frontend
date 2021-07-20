d4f4262072cdef148425b0dc70799fef484079bb
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-20 14:45:44 -0300

    :sparkles: add loader component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,12 +8,12 @@ import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome" />
