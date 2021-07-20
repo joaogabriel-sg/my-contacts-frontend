70baf8c51ca65841aaac8b4aedd477790c5ee906
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-20 07:27:18 -0300

    :sparkles: add link to navigate to pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
@@ -15,7 +17,7 @@ export function Home() {
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -37,49 +39,9 @@ export function Home() {
           </div>
 
           <div className="actions">
-            <a href="/" alt="Edit">
+            <Link to="/edit/123">
               <img src={edit} alt="Editar" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>João Gabriel</strong>
-              <small>instagram</small>
-            </div>
-            <span>joaogabriel@email.com</span>
-            <span>(88) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/" alt="Edit">
-              <img src={edit} alt="Editar" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Deletar" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>João Gabriel</strong>
-              <small>instagram</small>
-            </div>
-            <span>joaogabriel@email.com</span>
-            <span>(88) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/" alt="Edit">
-              <img src={edit} alt="Editar" />
-            </a>
+            </Link>
             <button type="button">
               <img src={trash} alt="Deletar" />
             </button>
