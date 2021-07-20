7a08de3b4b0f1f2dc4b8fe95ce5b49bf0a4436ef
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-20 06:11:04 -0300

    :sparkles: add routes component in app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import { Routes } from '../../Routes';
 
 import { Container } from './styles';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
