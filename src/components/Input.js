5b2e04ddb547b5291d359b639b4f48e16497f6ff
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-20 06:49:59 -0300

    :sparkles: add input component

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  background: #fff;
+  width: 100%;
+  height: 52px;
+
+  padding: 0 16px;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  outline: none;
+
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
