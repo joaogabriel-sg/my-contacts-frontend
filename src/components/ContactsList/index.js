618b0c88292878686a7b043eff224122591f1c83
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-20 05:59:20 -0300

    :sparkles: add contacts list cards

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,75 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Ordenar" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Gabriel</strong>
+              <small>instagram</small>
+            </div>
+            <span>joaogabriel@email.com</span>
+            <span>(88) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/" alt="Edit">
+              <img src={edit} alt="Editar" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Deletar" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Gabriel</strong>
+              <small>instagram</small>
+            </div>
+            <span>joaogabriel@email.com</span>
+            <span>(88) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/" alt="Edit">
+              <img src={edit} alt="Editar" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Deletar" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Gabriel</strong>
+              <small>instagram</small>
+            </div>
+            <span>joaogabriel@email.com</span>
+            <span>(88) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/" alt="Edit">
+              <img src={edit} alt="Editar" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Deletar" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
