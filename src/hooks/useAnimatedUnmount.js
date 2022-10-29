6cd611863c45ae0101ea5702672ce993c7c9ab15
João Gabriel <main.joaogabrielsg@gmail.com>
2022-10-29 17:27:43 -0300

    feat: create useAnimatedUnmount

---
+++ src/hooks/useAnimatedUnmount.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
+
+export function useAnimatedUnmount(visible) {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  const animatedElementRef = useRef(null);
+
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+    }
+
+    function handleAnimationEnd() {
+      setShouldRender(false);
+    }
+
+    const elementRef = animatedElementRef.current;
+    if (!visible && elementRef) {
+      elementRef.addEventListener('animationend', handleAnimationEnd);
+    }
+
+    return () => {
+      if (elementRef) {
+        elementRef.removeEventListener('animationend', handleAnimationEnd);
+      }
+    };
+  }, [visible]);
+
+  return {
+    shouldRender,
+    animatedElementRef,
+  };
+}
